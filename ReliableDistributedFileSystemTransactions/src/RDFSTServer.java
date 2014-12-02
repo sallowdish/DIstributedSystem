68bd98628acb2223a2734fa22f5cb466721fecaa
rza31 <rza31@sfu.ca>
2014-12-01 16:08:08 -0800

    Rename path to fileSystemPath for clarity

--- ReliableDistributedFileSystemTransactions/src/RDFSTServer.java
+++ ReliableDistributedFileSystemTransactions/src/RDFSTServer.java
@@ -14,7 +14,7 @@ import org.apache.commons.cli.CommandLine;
 public class RDFSTServer {
 	static enum MODE {PRIMARY,SECONDARY};
 	static final int MAX_CONNECTION=10;
-	public static Path path;
+	public static Path fileSystemPath;
 	public static String ip="127.0.0.1";
 	public static int port=8080;
 	public static MODE serverMode=MODE.SECONDARY;
@@ -27,9 +27,9 @@ public class RDFSTServer {
 		if(scanner.parse()){
 			try {
 				CommandLine cmd=scanner.cmd;
-				path=Paths.get(cmd.getOptionValue("dir"));
-				if (path.startsWith("~" + File.separator)) {
-				    path = Paths.get(System.getProperty("user.home") + path.toString().substring(1));
+				fileSystemPath=Paths.get(cmd.getOptionValue("dir"));
+				if (fileSystemPath.startsWith("~" + File.separator)) {
+				    fileSystemPath = Paths.get(System.getProperty("user.home") + fileSystemPath.toString().substring(1));
 				}
 				ip=cmd.getOptionValue("ip", "127.0.0.1");
 				port=Integer.parseInt(cmd.getOptionValue("port","8080"));
@@ -38,7 +38,7 @@ public class RDFSTServer {
 				System.err.println("Invalid Input");
 			}
 		}
-		System.out.println("ip:"+ip+"\nport:"+port+"\ndir:"+path);
+		System.out.println("ip:"+ip+"\nport:"+port+"\ndir:"+fileSystemPath);
 		
 		socket =new ServerSocket(port,MAX_CONNECTION,InetAddress.getByName(ip));
 		
@@ -49,9 +49,9 @@ public class RDFSTServer {
 			
 			try {
 				RequestMessage request=new RequestMessage(inFromClient);
-				(new Thread(new DFS(request,path,DFSSocket))).start();
+				(new Thread(new DFS(request,fileSystemPath,DFSSocket))).start();
 			} catch (Exception e) {
-				(new Thread(new DFS(null,path,DFSSocket))).start();
+				(new Thread(new DFS(null,fileSystemPath,DFSSocket))).start();
 			}
 			
 		}
